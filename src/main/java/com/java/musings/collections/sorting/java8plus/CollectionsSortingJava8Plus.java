759acf64c7675899d41f6b3e65c2f3442a84621e
hassanali <hassan.ali.cs@gmail.com>
2023-10-09 23:27:45 +0500

    added method to depict sorting lexicographically.

--- src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
+++ src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
@@ -25,7 +25,7 @@ public class CollectionsSortingJava8Plus {
         list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
         list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
 
-        Collections.sort(list, new Comparator<GymUser>() {
+        Collections.sort(list, new Comparator<>() {
             @Override
             public int compare(GymUser d1, GymUser d2) {
                 return Integer.compare(d1.getWeight(), d2.getWeight());
@@ -47,11 +47,25 @@ public class CollectionsSortingJava8Plus {
         list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
         list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
 
-        Collections.sort(list, (GymUser d1, GymUser d2) -> {
-            return Integer.compare(d1.getWeight(), d2.getWeight());
-        });
+        Collections.sort(list, (GymUser d1, GymUser d2) -> Integer.compare(d1.getWeight(), d2.getWeight()));
 
         assertEquals(list.get(0), new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+    }
+
+
+    @Test
+    public void collectionsSortingWithJava8Lambda_alphabetic() {
+
+        List<GymUser> list = new ArrayList<>();
+        list.add(new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
+        list.add(new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+        list.add(new GymUser(3, "Rana", 34, 110, 9, 2400, 450));
+        list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+        list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
+
+        Collections.sort(list, (GymUser user1, GymUser user2) -> user1.getName().compareTo(user2.getName()));
+
+        assertEquals(list.get(0), new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
     }
 
 }
