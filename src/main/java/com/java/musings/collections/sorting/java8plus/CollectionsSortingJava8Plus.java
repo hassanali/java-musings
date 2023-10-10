62714ad674731954ac2d279f2deca25a723b9ac4
hassanali <hassan.ali.cs@gmail.com>
2023-10-10 14:23:59 +0500

    added more methods to show different sorting flavours.

--- src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
+++ src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
@@ -63,9 +63,57 @@ public class CollectionsSortingJava8Plus {
         list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
         list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
 
-        Collections.sort(list, (GymUser user1, GymUser user2) -> user1.getName().compareTo(user2.getName()));
+        Collections.sort(list, (final GymUser user1, final GymUser user2) -> user1.getName().compareTo(user2.getName()));
 
         assertEquals(list.get(0), new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+    }
+
+
+    @Test
+    public void collectionsSortingWithJava8LambdaWithoutTypeDefinitions_alphabetic() {
+
+        List<GymUser> list = new ArrayList<>();
+        list.add(new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
+        list.add(new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+        list.add(new GymUser(3, "Rana", 34, 110, 9, 2400, 450));
+        list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+        list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
+
+        Collections.sort(list, (user1, user2) -> user1.getName().compareTo(user2.getName()));
+
+        assertEquals(list.get(0), new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+    }
+
+    @Test
+    public void collectionsSortingWithJava8Lambda_with_Comparator_comparing_alphabetic() {
+
+        List<GymUser> list = new ArrayList<>();
+        list.add(new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
+        list.add(new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+        list.add(new GymUser(3, "Rana", 34, 110, 9, 2400, 450));
+        list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+        list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
+
+        Collections.sort(list, Comparator.comparing(GymUser::getName));
+        list.sort(Comparator.comparing(GymUser::getName));
+
+        assertEquals(list.get(0), new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+    }
+
+    @Test
+    public void collectionsSortingWithJava8Lambda_with_Comparator_comparing_alphabetic_reversed() {
+
+        List<GymUser> list = new ArrayList<>();
+        list.add(new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
+        list.add(new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+        list.add(new GymUser(3, "Rana", 34, 110, 9, 2400, 450));
+        list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+        list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
+
+        Collections.sort(list, Comparator.comparing(GymUser::getName));
+        list.sort(Comparator.comparing(GymUser::getName).reversed());
+
+        assertEquals(list.get(0), new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
     }
 
 }
