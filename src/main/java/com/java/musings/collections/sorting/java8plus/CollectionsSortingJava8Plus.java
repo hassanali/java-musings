75d7c64a03792f9fe9aa46dbb44150efca9e01f6
hassanali <hassan.ali.cs@gmail.com>
2023-10-10 15:08:00 +0500

    added collectionsSortingWithJava8StreamAPI() to test the sorting functionality using Stream API's sorted(). This sorts the elements of the Stream according to the natural ordering. The Element must implement Comparable<T>

--- src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
+++ src/main/java/com/java/musings/collections/sorting/java8plus/CollectionsSortingJava8Plus.java
@@ -3,10 +3,8 @@ package com.java.musings.collections.sorting.java8plus;
 import com.java.musings.beans.GymUser;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -154,6 +152,23 @@ public class CollectionsSortingJava8Plus {
         Collections.sort(list, Comparator.comparing(GymUser::getName).thenComparing(GymUser::getAge));
 
         assertEquals(list.get(0), new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+    }
+
+
+    @Test
+    public void collectionsSortingWithJava8StreamAPI() {
+
+        List<GymUser> list = new ArrayList<>();
+        list.add(new GymUser(1, "Richard", 32, 99, 10, 2000, 300));
+        list.add(new GymUser(2, "Pen Li", 20, 70, 9, 3000, 800));
+        list.add(new GymUser(3, "Rana", 34, 110, 9, 2400, 450));
+        list.add(new GymUser(4, "Bob", 39, 104, 1, 1800, 500));
+        list.add(new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
+        list.add(new GymUser(6, "LaLi", 19, 90, 9, 1000, 690));
+
+        list = list.stream().sorted().collect(Collectors.toList());
+
+        assertEquals(list.get(0), new GymUser(5, "LaLi", 18, 85, 7, 1700, 600));
     }
 
 }
